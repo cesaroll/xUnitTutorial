55b3df63c4f9697206d9fe8d8e98adcfcbbf3d64
Cesar Lopez <clopezlerma@wayfair.com>
2021-02-05 15:50:56 +0100

    Using IClassFixture to inject same class into several test classes

--- xUnitTutorial.unitTests/Service/GuidGeneratorTests.cs
+++ xUnitTutorial.unitTests/Service/GuidGeneratorTests.cs
@@ -5,7 +5,11 @@ using xUnitTutorial.Service;
 
 namespace xUnitTutorial.unitTests.Service
 {
-    public class GuidGeneratorTestsOne : IClassFixture<GuidGenerator>, IDisposable
+    [CollectionDefinition("Guid generator")]
+    public class GuidGeneratorDefinition : IClassFixture<GuidGenerator>{}
+    
+    [Collection("Guid generator")]
+    public class GuidGeneratorTestsOne
     {
         private readonly GuidGenerator _guidGenerator;
         private readonly ITestOutputHelper _output;
@@ -30,14 +34,33 @@ namespace xUnitTutorial.unitTests.Service
             _output.WriteLine($"The guid was: {guid}");
         }
 
-        public void Dispose()
-        {
-            _output.WriteLine($"The class was disposed");
-        }
     }
     
+    [Collection("Guid generator")]
     public class GuidGeneratorTestsTwo
     {
+        private readonly GuidGenerator _guidGenerator;
+        private readonly ITestOutputHelper _output;
+
+        public GuidGeneratorTestsTwo(ITestOutputHelper output, GuidGenerator guidGenerator)
+        {
+            _output = output;
+            _guidGenerator = guidGenerator;
+        }
+
+        [Fact]
+        public void GuidTestOne()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
         
+        [Fact]
+        public void GuidTestTwo()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
+
     }
 }
